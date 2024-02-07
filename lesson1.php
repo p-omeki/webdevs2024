6f557ee75a814b762b531547c54a38dff9f685c3
Patrick Meki <partymeki@gmail.com>
2024-02-07 20:01:58 +0300

    Created the lesson1 as first demo...

---
+++ lesson1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+    <title>Lesson One</title>
+</head>
+<body>
+    <h1>Lesson One</h1>
+    <p>My first PHP page.</p>
+    <?php
+    // This is a single-line comment and my first PHP script
+    # This is also a single-line comment and am so excited to learn PHP
+    $X = 12;
+    $Y = 23;
+
+    #Computation section
+    $sum = $X + $Y;
+    $difference = $X - $Y;
+    $product = $X * $Y;
+    $division = $X / $Y;
+    $modulus = $X % $Y;
+
+    echo "The sum of $X and $Y is $sum <br>";
+    echo "The difference between $X and $Y is $difference <br>";
+    echo "The product of $X and $Y is $product <br>";
+    echo "The division of $X and $Y is $division <br>";
+    echo "The modulus of $X and $Y is $modulus <br>";
+
+        echo "Hello World!";
+    ?>
+    //The values of the variables, X and Y should come from a form
+</body>
+</html>
+```
