6b3376de4d278f37ac135dc5f09301be1eced223
p-omeki <partymeki@gmail.com>
2024-02-18 15:42:38 +0300

    Modified age.php file

--- age.php
+++ age.php
@@ -1,13 +1,10 @@
 <?php
-function checkAge($age) {
+function checkAge($age)
+{
     if ($age > 18) {
         echo "The student is an adult.";
     } else {
         echo "The student is still a child.";
     }
 }
-
-
 checkAge(10);
-?>
-
